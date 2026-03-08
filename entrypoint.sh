d0ce2bab4553313e12a08d2c5f52245323610989
George Garside <apps@georgegarside.com>
2026-03-08 15:40:11 +0000

    Increase timeout on nginx and php-fpm to 10 minutes

--- entrypoint.sh
+++ entrypoint.sh
@@ -7,14 +7,16 @@
 : "${FIREFLY_FLYCAST_URL:=http://localhost:8080}"
 : "${IMPORTER_APP_URL:=http://${IMPORTER_HOST}}"
 
-awk -v a="fastcgi_param APP_URL $FIREFLY_APP_URL;" -v h="$FIREFLY_HOST" '
+timeout=600
+
+awk -v a="fastcgi_param APP_URL $FIREFLY_APP_URL; proxy_connect_timeout $timeout; proxy_send_timeout $timeout; proxy_read_timeout $timeout; send_timeout $timeout;" -v h="$FIREFLY_HOST" '
 	/^[[:space:]]*server_name/ { print "\tserver_name " h ";" ; next }
 	/location ~ \\.php\$/ { print; inphp=1; next }
 	inphp && /fastcgi_pass/ { print; print "\t\t" a; inphp=0; next }
 	{ print }
 ' /etc/nginx/sites-available/default >/etc/nginx/sites-enabled/default
 
-awk -v a="fastcgi_param APP_URL $IMPORTER_APP_URL;" -v f="fastcgi_param FIREFLY_III_URL $FIREFLY_FLYCAST_URL;" -v h="$IMPORTER_HOST" -v r="/var/www/importer/public" '
+awk -v a="fastcgi_param APP_URL $IMPORTER_APP_URL; proxy_connect_timeout $timeout; proxy_send_timeout $timeout; proxy_read_timeout $timeout; send_timeout $timeout;" -v f="fastcgi_param FIREFLY_III_URL $FIREFLY_FLYCAST_URL;" -v h="$IMPORTER_HOST" -v r="/var/www/importer/public" '
 	/^[[:space:]]*listen/ { gsub(/default_server/,""); gsub(/[ \t]+;/,";"); sub(/[ \t]+$/,""); print; next }
 	/^[[:space:]]*server_name/ { print "\tserver_name " h ";" ; next }
 	/^[[:space:]]*root/ { print "\troot " r ";" ; next }
@@ -24,6 +26,7 @@ awk -v a="fastcgi_param APP_URL $IMPORTER_APP_URL;" -v f="fastcgi_param FIREFLY_
 ' /etc/nginx/sites-available/default >/etc/nginx/sites-enabled/importer
 
 sed -i -E 's~.*open_basedir.*~php_admin_value[open_basedir] = /var/www:/dev/stdout:/tmp~' /etc/php/*/fpm/pool.d/www.conf
+sed -i -E 's~.*max_execution_time.*~php_admin_value[max_execution_time] = '"$timeout"'~' /etc/php/*/fpm/pool.d/www.conf
 
 if [ -n "$FLY_IMAGE_REF" ]; then
 	if [ "$(< storage/ref)" == "$FLY_IMAGE_REF" ]; then
